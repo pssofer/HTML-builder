b3db746d05c89f9515107ea880ca887ddfb005d9
Павел <pavel@MacBook-Air-Pavel.local>
2022-05-24 10:44:51 +0300

    feat:update index.js in 04-copy-directory and 05-merge-styles

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.rm(__dirname + '/files-copy/', { recursive: true, force: true }, () => {
+  fs.mkdir(__dirname + '/files-copy/', () => {
+    fs.readdir(__dirname + '/files', (err, files) => {
+      files.forEach(file => {
+        const readStream = fs.createReadStream(__dirname + `/files/${file}`, {encoding: 'utf8'});
+        const writeStream = fs.createWriteStream(__dirname + `/files-copy/${file}`, {encoding: 'utf8'});
+        readStream.on('data', (data) => {
+          writeStream.write(data);
+        });
+      });
+    });
+  });
+});
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const projectDist = path.join(__dirname, 'project-dist');
 const styles = path.join(__dirname, 'styles');
+const readstyle = fs.createWriteStream(path.join(projectDist, 'bundle.css'));
 
 fs.readdir(styles, { withFileTypes: true }, (error, result) => {
   if (error) return console.error(error.message);
@@ -10,7 +11,7 @@ fs.readdir(styles, { withFileTypes: true }, (error, result) => {
     if (path.extname(file.name) === '.css') {
       fs.readFile(path.join(styles, file.name), (error, data) => {
         if (error) return console.error(error.message);
-        fs.createWriteStream(path.join(projectDist, 'bundle.css')).write(data);
+        readstyle.write(data);
       });
     }
   });
